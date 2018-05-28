c68bb313800fab767879ef5df8655d81240e77a6
pat <patrick.jappsen@digidrom.de>
2018-05-28 18:24:27 +0200

    First naive implementation to pass the test.

--- Hash.pm
+++ Hash.pm
@@ -7,19 +7,29 @@ package Hash;
 
 
 sub new {
-    return bless {}, shift;
+    my $self = shift;
+    $self = {
+        buckets => [ [], [], [], [], [], [], [] ],
+    };
+    bless  $self;
+    return $self;
 }
 
 
 sub store {
     my ($self, $key, $value) = @_;
+    my $buckets = $self->{buckets};
+    my ($bucket, $entry) = (0, 0);
+    $buckets->[$bucket][$entry][1] = $value;
     return $self;
 }
 
 
 sub fetch {
     my ($self, $key) = @_;
-    return $self;
+    my $buckets = $self->{buckets};
+    my ($bucket, $entry) = (0, 0);
+    return $buckets->[$bucket][$entry][1];
 }
 
 
